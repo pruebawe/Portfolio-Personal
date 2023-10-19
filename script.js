f468870d50c8e6e18c73dc3aca2a2296f66e7996
Miguel Lorente <eldknito.10@gmail.com>
2023-10-19 11:36:04 -0300

    se finalizo los efectos con javascript

---
+++ script.js
@@ -0,0 +1,41 @@
+let menuVisible = false;
+// Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //ocultar el menu una vez que selecciono una opción
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+//Función que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupa");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+//detecto el scrolling para apicar la animación de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
